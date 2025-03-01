0100947fb6257a5bc187b34483b253620dd349a4
SumitGarg <garg10sumit@gmail.com>
2025-03-01 12:39:29 +0530

    unique element so no worry of duplicate subset

---
+++ Recursion/Question/all_subset_genrate_uniqe.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void func(int i , int n, vector<vector<int>>&ans,vector<int> &nums , vector<int>&subset){
+        if(i == n){
+            ans.push_back(subset);
+            return;
+        }
+        subset.push_back(nums[i]);
+        func(i+1,n,ans,nums,subset);
+        subset.pop_back();
+        // remove dubplicate element
+        func(i+1,n,ans,nums,subset);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> subset;
+        func(0,n,ans,nums,subset);
+        return ans;
+    }
+};
+
