dff8f7165d580613df29d068d911c6f82ffb9daf
SumitGarg <garg10sumit@gmail.com>
2025-03-03 19:07:31 +0530

    same approch do explore undo

--- Recursion/Question/Combination_sum_2.cpp
+++ Recursion/Question/Combination_sum_2.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> ans;  
-    vector<int> curr;         
+    vector<vector<int>> ans;
+    vector<int> curr;
 
-    void solve(vector<int>& arr, int target, int idx) {
-        if (target < 0) return; 
+    void solve(vector<int> &arr, int target, int idx)
+    {
+        if (target < 0)
+            return;
 
-        if (target == 0) { 
+        if (target == 0)
+        {
             ans.push_back(curr);
             return;
         }
 
-        for (int i = idx; i < arr.size(); i++) {
-            if (i > idx && arr[i] == arr[i - 1]) {  // Skip duplicates
+        for (int i = idx; i < arr.size(); i++)
+        {
+            if (i > idx && arr[i] == arr[i - 1])
+            { // Skip duplicates
                 continue;
             }
 
-            curr.push_back(arr[i]); // Choose the current number
+            curr.push_back(arr[i]);             // Choose the current number
             solve(arr, target - arr[i], i + 1); // Move to the next index
-            curr.pop_back(); // Backtrack
+            curr.pop_back();                    // Backtrack
         }
     }
 
-    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
-        sort(arr.begin(), arr.end()); // Sort to handle duplicates
+    vector<vector<int>> combinationSum2(vector<int> &arr, int target)
+    {
+        sort(arr.begin(), arr.end());
         solve(arr, target, 0);
         return ans;
     }
