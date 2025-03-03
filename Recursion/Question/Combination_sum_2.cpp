48d025c92d5e75110b7c73d8b509acf3b7ea54f1
SumitGarg <garg10sumit@gmail.com>
2025-03-03 19:06:30 +0530

    add the element till target will be zero

---
+++ Recursion/Question/Combination_sum_2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> ans;  
+    vector<int> curr;         
+
+    void solve(vector<int>& arr, int target, int idx) {
+        if (target < 0) return; 
+
+        if (target == 0) { 
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+            if (i > idx && arr[i] == arr[i - 1]) {  // Skip duplicates
+                continue;
+            }
+
+            curr.push_back(arr[i]); // Choose the current number
+            solve(arr, target - arr[i], i + 1); // Move to the next index
+            curr.pop_back(); // Backtrack
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        sort(arr.begin(), arr.end()); // Sort to handle duplicates
+        solve(arr, target, 0);
+        return ans;
+    }
+};
