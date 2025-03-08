2db2c5de4e124a562f4b08396ba0f2d44fe84384
SumitGarg <garg10sumit@gmail.com>
2025-03-08 18:46:51 +0530

    using max heap

---
+++ Heap/1962_stonesCal.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq;
+        
+        
+        for (int it : piles) {
+            pq.push(it);
+        }
+        
+        
+        while (k--) {
+            int v = pq.top();
+            pq.pop();
+            int a = v / 2;
+            pq.push(v - a); // Push back the remaining stones
+        }
+        
+        
+        int sum = 0;
+        while (!pq.empty()) { 
+            sum += pq.top();
+            pq.pop();
+        }
+        
+        return sum;
+    }
+};
